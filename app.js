45062076af94fc4f16d607a75a371c8ca0643086
daveliew <daveliew@gmail.com>
2021-06-12 01:09:47 +0800

    created Bell class. need to resolve bell generation. improved snow render

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
 const gravityPull = -0.7;
+const numBells = 5;
 let hue = 0;
 let playerActivated = false;
 let gameFrame = 0;
@@ -16,6 +17,8 @@ const snow = {
   size: 3,
   amt: 15,
 };
+
+const bellArray = [];
 
 //! MAIN
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -34,10 +37,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
   class Snow {
     constructor() {
       this.x = Math.floor(Math.random() * bg.width);
-      this.y = Math.floor((Math.random() * gravityPull) / 2) + 1;
+      this.y = Math.floor(Math.random()) + 1;
       this.size = Math.floor(Math.random() * snow.size) + 1;
       this.speedX = Math.random() * 3 - 1.5; //* create -ve and +ve vector
-      this.speedY = Math.random() * 2 + 0.5;
+      this.speedY = Math.random() * gravityPull + 1;
       this.color = `hsl(${hue}, 100%, 50%)`;
     }
     update() {
@@ -52,6 +55,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   }
 
+  let preventSameBellX = 0; //? check on this
+
+  class Bell {
+    constructor() {
+      // const r = Math.floor(Math.random() * numBells); //?fix code
+      this.x = Math.random() * canvas.width;
+      this.y = 0;
+      this.color = "white";
+    }
+    update() {
+      this.x += Math.random() * 1 - 0.5;
+      this.y -= gravityPull; //? code looks suspect, revisit!
+    }
+    draw() {
+      ctx.fillStyle = this.color;
+      ctx.beginPath(); //* like a paint path
+      ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+      ctx.fill();
+    }
+    hasCollided() {}
+  }
   //* Generate Player
 
   class Player {
@@ -130,6 +154,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   };
 
+  //? fix the bell code to have one bell come down every X frames in random X pos
+  //* Generate bell
+  const generateBell = () => {
+    for (let i = 0; i < numBells; i++) {
+      bellArray.push(new Bell());
+    }
+  };
+
+  const bellRender = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].update();
+      arr[i].draw();
+    }
+  };
+
   //* Animate / Game loop
   const player = new Player();
   console.log("🚀 ~ file: app.js ~ line 92 ~ player", player);
@@ -141,7 +180,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
     player.draw();
-    player.speedY += 1.5; //? is this line useful?
+    player.speedY += 1.5;
+
+    //* bell code
+    bellRender(bellArray);
+    if (gameFrame % 300 === 0) {
+      generateBell();
+    }
 
     //* snow code
     bgCtx.clearRect(0, 0, bg.width, bg.height);
